e1cac452095fc7aeb0effd959ca069c758489ee2
gushizone <gushizone@gmail.com>
2022-11-17 10:58:14 +0800

    mybatis-plus+

--- demo-mybatis-plus/src/main/java/tk/gushizone/mybatisplus/mapper/BasePlusMapper.java
+++ demo-mybatis-plus/src/main/java/tk/gushizone/mybatisplus/mapper/BasePlusMapper.java
@@ -26,12 +26,12 @@ public interface BasePlusMapper<T> extends BaseExMapper<T> {
     }
 
     default boolean saveBatch(Collection<T> list, int batchSize) {
-        // todo 不可行, 这个 this 为代理对象
-        String sqlStatement =SqlHelper.getSqlStatement(this.getClass(), SqlMethod.INSERT_ONE);
-        return SqlHelper.executeBatch(this.currentModelClass(), LogFactory.getLog(getClass()), list, batchSize, (sqlSession, entity) -> sqlSession.insert(sqlStatement, entity));
-//        return SqlHelper.executeBatch(this.currentModelClass(), LogFactory.getLog(getClass()), list, batchSize, (sqlSession, entity) -> this.insert(entity));
+        Class<?> mapperClass = this.getClass().getInterfaces()[0];
+        String sqlStatement = SqlHelper.getSqlStatement(mapperClass, SqlMethod.INSERT_ONE);
+        return SqlHelper.executeBatch(this.currentModelClass(), LogFactory.getLog(mapperClass), list, batchSize, (sqlSession, entity) -> sqlSession.insert(sqlStatement, entity));
     }
 
+    @SuppressWarnings("unchecked")
     default Class<T> currentModelClass() {
         return (Class<T>) ReflectionKit.getSuperClassGenericType(this.getClass(), BasePlusMapper.class, 0);
     }
